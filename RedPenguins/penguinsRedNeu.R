d755908f6640e57d8609ae94b7e4c58900722708
BGEscalante <b_bryan1363@hotmail.com>
2024-04-17 23:44:16 -0600

    update Red Neuronal

--- RedPenguins/penguinsRedNeu.R
+++ RedPenguins/penguinsRedNeu.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(caret)
 library(neuralnet)
-
+library(ggplot2)
 datos = penguins
 datos = datos[complete.cases(datos), ]
 muestra = createDataPartition(datos$species, p=0.8, list=F)
@@ -21,3 +21,29 @@ decodificarCol = apply(prediccion,1, which.max)
 codificado = data_frame(decodificarCol)
 codificado = mutate(codificado, especie=recode(codificado$decodificarCol, "1" = "Adelie", "2" = "Chinstrap", "3" = "Gentoo"))
 test$Especie.Pred = codificado$especie
+
+datos %>%
+  count(species)
+datos%>%
+  group_by(species)%>%
+  summarize(across(where(is.numeric),mean,na.rm = TRUE))
+
+plot(datos)
+
+datos%>% 
+  count(species) %>%
+  ggplot() + geom_col(aes(x = species, y = n, fill = species)) 
+  geom_label(aes(x = species, y =n, label= n )) + 
+    scale_fill_manual(values = c('darkorange','purple', 'cyan4')) + 
+    theme_minimal() + 
+    labs(title = 'DISTRIBUCION DE PINGUINOS POR ESPECIE')
+  
+datos %>%
+  drop() %>%
+  count(sex,species) %>%
+  ggplot() + geom_col(aes(x = species, y = n, fill= species)) +
+  geom_label(aes(x=species, y=n, label = n)) +
+  scale_fill_manual(values = c('darkorange','purple', 'cyan4')) +
+  facet_wrap(~sex) +
+  theme_minimal() +
+  labs(title = 'Especies de pinguinos ~ sexo')
