ebcce9c4e2b28421dfc4c40e9762403f25ab338f
BGEscalante <b_bryan1363@hotmail.com>
2024-05-07 00:25:59 -0600

    Mejora de error en RedNeuronal y codigo en jupyter

--- RedPenguins/penguinsRedNeu.R
+++ RedPenguins/penguinsRedNeu.R
@@ -8,8 +8,14 @@ datos = datos[complete.cases(datos), ]
 muestra = createDataPartition(datos$species, p=0.8, list=F)
 train = datos[muestra,]
 test = datos[-muestra,]
-red.neuronal = neuralnet(species ~ bill_length_mm + bill_depth_mm + flipper_length_mm + body_mass_g , data = train, hidden = c(2,3))
-red.neuronal$act.fct
+
+# Escalado de datos
+train_scaled <- as.data.frame(scale(train[, c("bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g")]))
+train_scaled$species <- train$species
+
+red.neuronal = neuralnet(species ~ bill_length_mm + bill_depth_mm + flipper_length_mm + body_mass_g, 
+                         data = train_scaled, hidden = c(4,5,2))
+#red.neuronal$act.fct
 #graficacion de la red neuronal 
 plot(red.neuronal)
 #aplicar el conjunto de prueba a la red para predecir la especie 
@@ -47,3 +53,59 @@ datos %>%
   facet_wrap(~sex) +
   theme_minimal() +
   labs(title = 'Especies de pinguinos ~ sexo')
+
+##Tamaño tux por specie
+ggplot(data = penguins,
+       aes(x = flipper_length_mm,
+           y = body_mass_g)) + 
+  geom_point(aes(color = species, 
+                 shape = species),
+                  size = 3,
+                  alpha = 0.8) +
+    #theme_minimal()+
+    scale_color_manual(values = c("darkorange","purple", "cyan4")) +
+    labs(title ="Tamaño del pinguino, Palmer Station LTER",
+         subtitle = "Longitud de aleta y Masa corporal para pinguinos Adelie, Chinstrap y Gentoo",
+         x = "Longitud de aleta (mm)",
+         y = "Masa coroporal(g)",
+         color = "especie de pinguino",
+         shape = "especie de pinguino") + 
+    theme_minimal()
+
+##Tamaño tux por isla
+ggplot(data = penguins,
+       aes(x = flipper_length_mm,
+           y = body_mass_g)) + 
+  geom_point(aes(color = island, 
+                 shape = species),
+             size = 3,
+             alpha = 0.8) +
+  #theme_minimal()+
+  scale_color_manual(values = c("darkorange","purple", "cyan4")) +
+  labs(title ="Tamaño del pinguino, Palmer Station LTER",
+       subtitle = "Longitud de aleta y Masa corporal segun cada isla",
+       x = "Longitud de aleta (mm)",
+       y = "Masa coroporal(g)",
+       color = "isla del pinguino",
+       shape = "especie de pinguino") + 
+  theme_minimal()
+
+##Tamaño tux por tam pico
+ggplot(data = penguins,
+       aes(x = flipper_length_mm,
+           y = bill_length_mm)) + 
+  geom_point(aes(color = species, 
+                 shape = species),
+             size = 3,
+             alpha = 0.8) +
+  #theme_minimal()+
+  scale_color_manual(values = c("darkorange","purple", "cyan4")) +
+  labs(title ="Tamaño del pinguino, Palmer Station LTER",
+       subtitle = "Longitud de Aleta y longitud de pico segun tamaño del pinguino",
+       x = "Longitud de aleta (mm)",
+       y = "Tamaño de pico (mm)",
+      color = "especie de pinguino",
+       shape = "especie de pinguino") + 
+  theme_minimal()
+
+plot(red.neuronal)
